cc570336a26152e45deb942f1d7ccbaf872776e5
Kucherenko Sergey <buhanka1994@gmail.com>
2018-08-30 18:28:14 +0300

    Change redirects

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
-    <Header />
     <Content />
-     <Footer />
   </div>
 </template>
 
 <script>
-import Header from "@/components/sections/main/Header";
 import Content from "@/components/sections/main/Content";
-import Footer from "@/components/sections/main/Footer";
 
 export default {
   components: {
-    Header,
-    Content,
-    Footer
+    Content
   }
 };
 </script>
